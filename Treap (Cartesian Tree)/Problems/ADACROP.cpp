3a2c2c4d967d8053e9b61737c58cab1ae722c4f7
Andrey Stoev <akstoev@uni-sofia.bg>
2021-09-02 23:37:02 +0300

    Create ADACROP.cpp

---
+++ Treap (Cartesian Tree)/Problems/ADACROP.cpp
@@ -0,0 +1,144 @@
+// https://www.spoj.com/problems/ADACROP/
+
+// Idea: Make a Treap data structure for every vegetable type, using it's unique position as key
+
+// Time complexity: O(N+Q*log^2(N)), where N is the number of all vegetables and Q is the number of queries
+// Space complexity: O(N)
+
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+typedef struct Node* pnode;
+
+struct Node {
+    int key, prior, cnt; // cnt nodes in subtree with the curr root (incl. root)
+    pnode l, r;
+
+    Node(int key) : key(key), prior(rand()), cnt(1), l(nullptr), r(nullptr) {}
+};
+
+struct Treap {
+private:
+    pnode root;
+
+    int cnt(pnode t) {
+        return t ? t->cnt : 0;
+    }
+
+    void updCnt(pnode t) {
+        if (t) {
+            t->cnt = 1 + cnt(t->l) + cnt(t->r);
+        }
+    }
+
+    void split(pnode t, int key, pnode &l, pnode &r) {
+        if (!t) {
+            l = r = nullptr;
+        } else if (t->key <= key) {
+            split(t->r, key, t->r, r), l = t;
+        } else {
+            split(t->l, key, l, t->l), r = t;
+        }
+        updCnt(t);
+    }
+
+    void insert(pnode &t, pnode it) {
+        if (!t) {
+            t = it;
+        } else if (it->prior <= t->prior) {
+            insert(t->key < it->key ? t->r : t->l, it);
+        } else {
+            split(t, it->key, it->l, it->r), t = it;
+        }
+        updCnt(t);
+    }
+
+    void merge(pnode &t, pnode l, pnode r) {
+        if (!l || !r) {
+            t = l ? l : r;
+        } else if (l->prior <= r->prior) {
+            merge(r->l, l, r->l), t = r;
+        } else {
+            merge(l->r, l->r, r), t = l;
+        }
+        updCnt(t);
+    }
+
+    void erase(pnode &t, int key) {
+        if (!t) {
+            return;
+        }
+        if (t->key == key) {
+            pnode temp = t;
+            merge(t, t->l, t->r);
+            delete temp;
+        } else {
+            erase(key <= t->key ? t->l : t->r, key);
+        }
+        updCnt(t);
+    }
+
+    int queryFirstVersion(pnode t, int key) {
+        pnode l(nullptr), r(nullptr);
+        split(t, key - 1, l, r);
+        int res = (l ? l->cnt : 0);
+        merge(t, l, r);
+        return res;
+    }
+
+    int querySecondVersion(pnode t, int key) {
+        if (!t) {
+            return 0;
+        } else if (t->key < key) {
+            return 1 + cnt(t->l) + querySecondVersion(t->r, key);
+        } else {
+            return querySecondVersion(t->l, key);
+        }
+    }
+
+public:
+    Treap() : root(nullptr) {};
+
+    void insert(int key) {
+        insert(this->root, new Node(key));
+    }
+
+    void erase(int key) {
+        erase(this->root, key);
+    }
+
+    int query(int key) {
+        //return queryFirstVersion(this->root, key);
+        return querySecondVersion(this->root, key);
+    }
+};
+
+int main() {
+    int N, Q, pos, vegType;
+    scanf("%d %d", &N, &Q);
+
+    vector<int> a(N);
+    map<int, Treap> vegTypeTreap;
+
+    for (int i = 0; i < N; ++i) {
+        scanf("%d", &a[i]);
+        vegTypeTreap[a[i]].insert(i);
+    }
+
+    while (Q--) {
+        scanf("%d %d", &pos, &vegType);
+        Treap *t = &vegTypeTreap[vegType];
+        printf("%d\n", t->query(pos));
+        if (a[pos] != vegType) {
+            vegTypeTreap[a[pos]].erase(pos);
+            t->insert(pos);
+            a[pos] = vegType;
+        }
+    }
+
+    return 0;
+}
