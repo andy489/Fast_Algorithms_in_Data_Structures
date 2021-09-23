3299ccbe31aa87bce56b1ab71cf2822fc071752e
Andrey Stoev <akstoev@uni-sofia.bg>
2021-09-24 00:34:38 +0300

    Update van Emde Boas Tree.cpp

--- van Emde Boas Trees/van Emde Boas Tree.cpp
+++ van Emde Boas Trees/van Emde Boas Tree.cpp
@@ -14,10 +14,10 @@
     succ(x) = -1, if x is larger than the largest element in the collection
     succ(x) = y, where y is the smallest element, larger than x in the collection
     
-    Space	    O(U)
-    Member      O(log log U)
-    Insert      O(log log U)
-    Delete      O(log log U)
+    Space	        O(U)
+    Member          O(log log U)
+    Insert          O(log log U)
+    Delete          O(log log U)
     Predecessor     O(log log U)
     Successor       O(log log U)
     Minimum         O(1)
